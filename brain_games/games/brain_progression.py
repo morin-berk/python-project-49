27246adb4376e03cb00729adf18acd73b4d264b3
Kseniia Bekisheva <kseniia_bekisheva@epam.com>
2024-03-20 18:02:49 +0300

    Refactor brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,7 +1,26 @@
 from random import randrange
 from brain_games.cli import welcome_user
 from brain_games.scripts.game_logic import check_player_answer
-from brain_games.consts import PROGRESSION_RULES, MIN_RANGE_LENGTH, MAX_RANGE_LENGTH
+from brain_games.consts import PROGRESSION_RULES, \
+    MIN_RANGE_LENGTH, MAX_RANGE_LENGTH
+
+
+def get_correct_answer_and_progression():
+    progression_len = randrange(5, 10)
+    missing_el_index = randrange(0, progression_len - 1)
+    progression_step = randrange(1, 10)
+    first_el = randrange(MIN_RANGE_LENGTH, MAX_RANGE_LENGTH - progression_step)
+    progression = []
+
+    for el in range(first_el,
+                    first_el + progression_step * progression_len,
+                    progression_step):
+        progression.append(el)
+
+    correct_answer = progression[missing_el_index]
+    progression[missing_el_index] = '..'
+
+    return correct_answer, progression
 
 
 def play_progression():
@@ -17,20 +36,7 @@ def play_progression():
     rounds = 0
 
     while -1 < rounds < 3:
-
-        progression_len = randrange(5, 10)
-        missing_el_index = randrange(0, progression_len - 1)
-        progression_step = randrange(1, 10)
-        first_el = randrange(MIN_RANGE_LENGTH, MAX_RANGE_LENGTH - progression_step)
-        progression = []
-
-        for el in range(first_el,
-                        first_el + progression_step * progression_len,
-                        progression_step):
-            progression.append(el)
-
-        correct_answer = progression[missing_el_index]
-        progression[missing_el_index] = '..'
+        correct_answer, progression = get_correct_answer_and_progression()
         question_expression = ' '.join(map(str, progression))
 
         rounds = check_player_answer(correct_answer,
