0551371851e7cb1d125470907bf39e91f617261b
Kseniia Bekisheva <kseniia_bekisheva@epam.com>
2024-03-26 12:08:03 +0300

    Refactor brain-calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -13,19 +13,15 @@ def get_correct_answer(rand_sign, num1, num2):
     return correct_answer
 
 
-def get_rand_sign_and_two_nums():
-    num1, num2 = randrange(MIN_RANGE_LENGTH, MAX_RANGE_LENGTH), randrange(
-        MIN_RANGE_LENGTH, MAX_RANGE_LENGTH)
-    random_sign = choice(MATH_SIGNS)
-    return random_sign, num1, num2
-
-
 def play_calc():
     """
     The game asks a player an answer to the simple
     arithmetical expression (+, - or *).
     """
-    random_sign, num1, num2 = get_rand_sign_and_two_nums()
+    num1, num2 = randrange(MIN_RANGE_LENGTH, MAX_RANGE_LENGTH), \
+                 randrange(MIN_RANGE_LENGTH, MAX_RANGE_LENGTH)
+    random_sign = choice(MATH_SIGNS)
+
     correct_answer = get_correct_answer(random_sign, num1, num2)
     question_expression = f'{str(num1)} ' \
                           f'{str(random_sign)} {str(num2)}'
